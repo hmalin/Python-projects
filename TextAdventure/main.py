263131a589100cd6ffaad38c174c0b53a4ccb563
Lloyd Malin <61097127+hmalin@users.noreply.github.com>
2024-05-05 03:18:54 -0700

    updated code - 5/5/24
    updated logic- convert user input to lower then check condition.

--- TextAdventure/main.py
+++ TextAdventure/main.py
@@ -19,9 +19,9 @@ print("!!!!!!!!!!!!!!!!!!")
 print('''You awaken in a strange place to sounds of distant screams....
 You need need to escape this place..... You see two paths> One to the left and one to the right.
 ''')
-first_room = input("Which one do you choose Enter: Left or Right. ")
+first_room = input("Which one do you choose Enter: Left or Right. \n ").lower()
 
-if first_room == "Left" or first_room== "left":
+if first_room == "left":
     print("You make it to the next room ands see a strange gun on the table ")
     print('''
                 _____
@@ -39,8 +39,8 @@ if first_room == "Left" or first_room== "left":
     
     ''')
 
-    item_pickup = input("Do you pick it up? Enter Yes or No ")
-    if item_pickup == "Yes" or item_pickup== "yes":
+    item_pickup = input("Do you pick it up? Enter Yes or No \n ").lower()
+    if item_pickup== "yes":
         print("You run into a horifying creature")
         print('''
         
@@ -61,12 +61,12 @@ if first_room == "Left" or first_room== "left":
         
         
         ''')
-        choice = input("shoot it? Yes or No ")
-        if choice == "yes" or choice =="Yes":
+        choice = input("shoot it? Yes or No \n").lower()
+        if choice == "yes":
             print(" You felled the creature and move to a room with an escape shuttle")
             print("You find a control panel with four buttons which look to unlock the shuttle ")
-            last_choice = input("which button do you press Red , Blue or Yellow ?: Enter your choice ")
-            if last_choice == "Yellow" or last_choice == "yellow":
+            last_choice = input("which button do you press Red , Blue or Yellow ?: Enter your choice \n ").lower()
+            if last_choice == "yellow":
                 print("The Ship is unlocked! You board and make your escape! ")
                 print('''
                               _,--=--._
@@ -83,7 +83,7 @@ if first_room == "Left" or first_room== "left":
                  *************ESCAPE SUCCESS**********
                  ''')
 
-            elif last_choice == "Red" or last_choice == "red":
+            elif last_choice == "red":
                 print("The room explodes!")
                 print(''' 
                 
@@ -99,7 +99,7 @@ if first_room == "Left" or first_room== "left":
                        (../( )\.))
                 *******GAME OVER*********
                 ''')
-            elif last_choice == "Blue" or last_choice == "blue":
+            elif last_choice == "blue":
                 print("You opened the airlock and are sucked into space")
                 print('''
                                 .              +   .                .   . .     .  .
